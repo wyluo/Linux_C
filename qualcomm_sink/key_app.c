3285431ad8e96f42234a0a7ba8965f27298ec3c3
wyluo <fortwater969@163.com>
2022-04-05 13:57:50 +0800

    update button moudle used for button

--- qualcomm_sink/key_app.c
+++ qualcomm_sink/key_app.c
@@ -6,3 +6,10 @@ const KEY_MENU_T key_app_menu_table_t[] ={
 };
 
 const unsigned char key_app_menu_table_num = sizeof(key_app_menu_table_t) / sizeof(*key_app_menu_table_t);//计算key_app_menu_table_t 的大小
+
+void a_KeyEvtTranslate(void)
+{
+    T_KEY_PRESS_HANDLE_T key_handle;
+    
+    if()
+}
